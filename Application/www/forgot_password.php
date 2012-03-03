49beb2a4fa23bbb7ad38c30ddf1ada9282075fc4
churkl <churkl@gmail.com>
2012-03-03 22:59:19 +0000

    Small correction on how to get a user

--- Application/www/forgot_password.php
+++ Application/www/forgot_password.php
@@ -8,7 +8,15 @@ if (!empty($_POST)){
 	$email = htmlentities($_POST['email']);
 	
 	try {
-		$user = Factory::getView(new UserKey($email));
+		$results = User::getOptions(array('USERNAME'=>$email));
+		
+		if (sizeof($reults) > 1) {
+			// This is an error where we have more than 1 user with the same email, should never happen
+		} else if (sizeof($reults) === 0) {
+			// This is when we can't find the user
+		} else {
+			$user = Factory::getView(new UserKey($results[0]['id']));
+		}
 		
 	} catch (NoResultException $e) {
 		echo 'User with username: ' . $email . ' was not found';
