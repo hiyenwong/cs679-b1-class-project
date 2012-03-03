5a3de4093a47ced41a3bd0871b8f852bd558337c
churkl@gmail.com <churkl@gmail.com@e7e56146-f8dc-8eb2-db1c-0f49c4865b40>
2012-03-03 22:27:55 +0000

    Fixed sources

--- trunk/Application/www/forgot_password.php
+++ trunk/Application/www/forgot_password.php
@@ -1,11 +1,24 @@
 <?php
-	require_once '../includes/config.inc';
+require_once '../includes/config.inc';
+require_once 'user.inc';
+require_once 'factory.inc';
+require_once 'view.inc';
+
+if (!empty($_POST)){
+	$email = htmlentities($_POST['email']);
 	
-	if (!empty($_POST)){
-	
+	try {
+		$user = Factory::getView(new UserKey($email));
+		
+	} catch (NoResultException $e) {
+		echo 'User with username: ' . $email . ' was not found';
+	} catch (Exception $e) {
+		echo 'Internal Error occurred, please email administrator for further assistance.';
 	}
 	
-	$smarty = new MySmarty($SMARTY_CONFIG);
-	$smarty->display('forgot_password.tpl');
+}
+
+$smarty = new MySmarty($SMARTY_CONFIG);
+$smarty->display('forgot_password.tpl');
 	
 ?>
