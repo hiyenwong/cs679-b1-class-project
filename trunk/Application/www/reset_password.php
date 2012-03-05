e89e80e62536f1dcc645578700dd464e11def7fb
vdineva@bu.edu <vdineva@bu.edu@e7e56146-f8dc-8eb2-db1c-0f49c4865b40>
2012-03-05 00:18:57 +0000

    Reset password functionality

--- trunk/Application/www/reset_password.php
+++ trunk/Application/www/reset_password.php
@@ -3,33 +3,51 @@ require_once '../includes/config.inc';
 require_once 'user.inc';
 require_once 'factory.inc';
 require_once 'view.inc';
+require_once 'transaction.inc';
+
+$smarty = new MySmarty($SMARTY_CONFIG);
 
 if (!empty($_POST)){
-	$email = htmlentities($_POST['email']);
 	
+	if ($_POST['new_password'] != $_POST['verify_new_password']) {
+		$err_message = 'The two passwords must match';
+		$smarty->assign('err_message', $err_message);
+		$smarty->display('reset_password.tpl');
+	} 
+	
+	//additional checks that the password is at least 8 chars etc
 	try {
-		$results = User::getUserByUserName(array('USERNAME'=>$email));
-
-		if (sizeof($results) > 1) {
-			// This is an error where we have more than 1 user with the same email, should never happen
-			$err_message = 'Internal Error occurred, please email administrator for further assistance.';
-		} else if (sizeof($results) === 0) {
-			// This is when we can't find the user
-			$err_message = 'User with username: ' . $email . ' was not found';
-		} else {
-			$user = Factory::getView(new UserKey($results[0]['id']));
-			$new_password_hash = $user->generateNewPassword();
-		
-
-			
+		$username = $_POST['username'];
+		$pid = $_POST['pid'];
+	
+		$result = User::checkAuthentication($username, $pid, true);
+		if ($result) {
+			$transaction = new Transaction(new MySqlDB());
+  			$transaction->start();
+  			
+			$results = User::getUserByUserName($username);
+			foreach ($results as $row) {
+  				// We know results has only one record so it is ok to foreach on this and break
+  				$user = Factory::getView(new UserKey($row['id']));
+  				break;
+  			}		
+			$new_password = $user->setPassword($_POST['new_password'], false);	
+			$transaction->commit();
 		}
 		
 	} catch (Exception $e) {
-		$err_message = 'Internal Error occurred, please email administrator for further assistance.';
+		//handle exception
 	}
 	
+	$smarty->display('reset_password.tpl');
+} else {
+	$username = $_GET['username'];
+	$pid = $_GET['pid'];
+		
+	$smarty->assign('username', $username);
+	$smarty->assign('pid', $pid);
+	$smarty->display('reset_password.tpl');	
 }
 
-//$smarty = new MySmarty($SMARTY_CONFIG);
-	
+
 ?>
