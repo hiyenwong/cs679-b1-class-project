3d1e7b8986ab825020cdd346640d511e3db2b6a5
jreimels <jreimels@bu.edu>
2012-03-19 02:25:46 +0000

    beginning work on login

--- Application/www/login.php
+++ Application/www/login.php
@@ -1,8 +1,33 @@
 <?php
 	require_once '../includes/config.inc';
+	require_once '../includes/access.inc';
 	
-	$smarty = new MySmarty($SMARTY_CONFIG);
-	
+	$requestedUrl = isset($_REQUEST['ref']) ? base64_decode(htmlentities($_REQUEST['ref'])) : $BASE_URL;
+    $submitted = isset($_REQUEST['submitted']);
+    $username = isset($_REQUEST['username']) ? htmlentities($_REQUEST['username']) : false;
+    $password = isset($_REQUEST['password']) ? htmlentities($_REQUEST['password']) : false;
+    
+    $smarty = new MySmarty($SMARTY_CONFIG);
+    
+    if ($submitted) { 
+        try {
+            $access = new Access();
+            if ($access->authenticate($username, $password)) {
+                header ("Location: " . $requestedUrl);
+                exit();
+            } else {
+                $smarty->assign('errorMessage', 'Username or Password is incorrect. Please try logging in again.');
+            }
+            
+        } catch (AccessDeniedException $e) {
+            header ('HTTP/1.1 401 Access Denied');
+            echo "AccessDeniedException: " . $e->getMessage ();
+        } catch (Exception $e) {
+            header ('HTTP/1.1 500 Internal Server Error');
+            echo "Exception: " . $e->getMessage ();
+        }
+    }
+    
 	$smarty->display('login.tpl');
 	
 ?>
