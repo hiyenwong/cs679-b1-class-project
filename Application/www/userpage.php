9a09307b47c4861cf12f5a038418f96bed49fec8
jreimels <jreimels@bu.edu>
2012-03-25 18:30:18 +0000

    updated userpage.php to use access class

--- Application/www/userpage.php
+++ Application/www/userpage.php
@@ -1,16 +1,18 @@
 <?php
 	session_start();
 	require_once '../includes/config.inc';
-	require_once 'user.inc';
+	require_once '../includes/user.inc';
+	require_once '../includes/access.inc';
 
 	$smarty = new MySmarty($SMARTY_CONFIG);
+    $access = new Access();
 
-	$user_temp = User::getUserByUserName($_SESSION['user']);
+	$user_temp = User::getUserByUserName($access->getUser());
 	$user = UserSource::getSource();
 	$user = $user->read(array('ID'=>$user_temp[0]['id']));
 
 	$smarty->assign('username', $user['first_name']);
 	$smarty->assign('left_menu', true);
 	$smarty->display('userpage.tpl');
-	
+
 ?>
