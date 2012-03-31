241d3458293a9ced720affcf3ea69d6fe409dedf
churkl@gmail.com <churkl@gmail.com@e7e56146-f8dc-8eb2-db1c-0f49c4865b40>
2012-03-31 13:13:06 +0000

    Contact controller rewriten, not tested yet.

--- trunk/Application/www/contact.php
+++ trunk/Application/www/contact.php
@@ -1,92 +1,61 @@
 <?php
 	require_once '../includes/config.inc';
 	require_once 'user.inc';
+	require_once 'access.inc';
 	require_once 'date.inc';
-    require_once 'factory.inc';
-    require_once 'view.inc';
+	require_once 'factory.inc';
+	require_once 'view.inc';
 	require_once 'transaction.inc';
-    require_once 'contact.inc';
-    
-    $smarty = new MySmarty($SMARTY_CONFIG);
-	
-    if (isset($_POST['submit'])) {
-	    $error = "";
-		
-		if (!empty($_POST)) {
-			try {
-				if (!empty($_POST['name'])) {
-					$name = htmlentities($_POST['name']);
-				}
-				else {
-					$error .= "Please enter your name. <br />";
-				}
-			
-				if (!empty($_POST['email'])) {
-					$email = htmlentities($_POST['email']);
-				    if (!preg_match("/^[_a-z0-9]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $email)){
-				  		$error .= "Please enter a valid email address. <br/>";
-					}
-				}
-				else {
-					$error .= "Please enter a valid email address. <br />";
-				}
-			
-				if (!empty($_POST['message'])) {
-					$message = htmlentities($_POST['message']);
-				}
-				else {
-					$error .= "Please enter a message. <br />";
-				}
-						
-				if (empty($error)) {
-					$from = 'From: ' . $name . ' <' . $email . '>';
-					$to = "qlahnn@bu.edu";
-					$subject = "iBuget Contact Form Message";
-					$content = $name . " has sent the iBudget team a message: \n" . $message;
-					mail($to,$subject,$content,$from);
-				}
-						
-				// inserting into table contact				
-				$t = new Transaction(new MySqlDB());
-				$t->start();
-				$u = false;
-				try {
-					$u = Factory::getView(new UserKey($user_id));
-				} catch (NoResultException $e) {
-					// no user logon
-					$u = false;
-				} catch (Exception $e) {
-				  echo 'Internal Error occurred, please email administrator for further assistance.';
-				}
-				
-				// check to see if user's email exists in database
-				if (!$u) {
-					$results = User::getUserByUserName($email);
-					foreach ($results as $id => $val) {
-						$u = Factory::getView(new UserKey($id));
-						break;
-					}
-				}
-			
-				$contact = Factory::createView(new ContactKey());
-				if ($u) {
-					$contact.setUser($u);
-				}
-				
-				$date = new Date();
-				$contact->setEmail($email);
-				$contact->setContent($message);
-				$contact->setDateSubmitted($date);
-				$t->commit();
+	require_once 'contact.inc';
 
-			} catch (Exception $e) {
-				if ($t && !$t->isComplete()) {
-    				$t->rollBack();
-				}
-				echo $error;
-			}
+	if (isset($_POST['submit'])) {
+		$user = false;
+
+		try {
+			$access = new Access();
+			$access->authenticate(null, null, false);
+			$user = $access->getUser();
+		} catch (AccessDeniedException $e) {
+			// Don't need to do anything, This just means user is not logged in.
 		}
+
+		try {
+			$name = isset($_REQUEST['name']) ? htmlentities($_REQUEST['name']) : false;
+			$email = isset($_REQUEST['email']) && preg_match("/^[_a-z0-9]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", $_REQUEST['email']) ?
+				htmlentities($_REQUEST['email']) : false;
+			$message = isset($_REQUEST['message']) ? htmlentities($_REQUEST['message']) : false;
+
+			// inserting into table contact
+			$transaction = new Transaction(new MySqlDB());
+			$transaction->start();
+
+			if (!$user) {
+				// User is not logged in, so trying to find user by his provided email
+				$temp = User::getUserByUserName($email);
+				if ($temp !== null) {
+					$user = $temp;
+				}
+			}
+
+			$contact = Factory::createView(new ContactKey());
+			if ($user) {
+				$contact.setUser($user);
+			}
+
+			$contact->setName($name);
+			$contact->setEmail($email);
+			$contact->setContent($message);
+			$contact->setDateSubmitted(new Date());
+			$transaction->commit();
+
+		} catch (Exception $e) {
+			if ($transaction && !$transaction->isComplete()) {
+				$transaction->rollBack();
+			}
+			echo "<PRE>A error had occured: " . $e->getMessage() . "\n" . $e->getTraceAsString() . "\n</PRE>";
+		}
+	} else {
+		$smarty->display('contact.tpl');
+		$smarty = new MySmarty($SMARTY_CONFIG);
 	}
-	
-	$smarty->display('contact.tpl');
 ?>
