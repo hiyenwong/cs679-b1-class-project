bfac5bcba01731037159ad43792c467b70b5ccfe
jreimels@bu.edu <jreimels@bu.edu@e7e56146-f8dc-8eb2-db1c-0f49c4865b40>
2012-04-09 00:42:50 +0000

    modified budget.inc and activity.inc getOption methods to return more data added jQueryUI css and js files added new method to Date.inc to get a the date from the 1st of the current month budget page is now fully functional (css changes still needed)

---
+++ trunk/Application/templates/budget_table.tpl
@@ -0,0 +1,20 @@
+<table>
+    <thead>
+        <tr>
+            <th>Category</th>
+            <th>Budgeted</th>
+            <th>Spent</th>
+            <th>&nbsp;</th>
+        </tr>
+    </thead>
+    <tbody id="budgetRows">
+        {foreach from=$budgets key=k item=v}
+            <tr>
+                <td>{$v.name}</td>
+                <td>${$v.amount}</td>
+                <td>${$spent.$k}</td>
+                <td><input type="button" value="Remove" onclick="removeBudgetItem(this, {$k})" /></td>
+            </tr>
+        {/foreach}                    
+    </tbody>
+</table>
--- trunk/Application/www/js/budget.js
+++ trunk/Application/www/js/budget.js
@@ -1,16 +1,26 @@
 $(document).ready(function () {
 
-    $("#addBudgetRow").dialog({autoOpen: false})
+    $("#addBudgetRow").dialog({autoOpen: false, title: "Add Budget Item"})
 
     $("#addBudget").click(function () {
         $("#addBudgetRow").dialog("open");
     });
     
     $("#saveBudgetItem").click(function() {
+    
+        if (!$("#addBudgeted").val().match(/^\d+(\.\d{2})?$/)) {
+            $("#addBudgetError").html("Please enter a valid number in the form of either <b>5</b> or <b>5.20</b>.");
+            return;
+        }
+        $("#addBudgetError").html("");
+    
         $.ajax({
             type: "POST",
             url: "budget.php",
-            data: $("#addBudgetRow form").serialize()
+            data: $("#addBudgetRow form").serialize(),
+            success: function(data) {
+                $("#budgetTable").html(data);
+            }
         });
         
         $("#addBudgeted").val("");
@@ -18,6 +28,8 @@ $(document).ready(function () {
     });
     
     $("#cancelBudgetItem").click(function() {
+        $("#addBudgetError").html("");
+        $("#addBudgeted").val("");
         $("#addBudgetRow").dialog("close");
     });
     
@@ -27,8 +39,11 @@ var removeBudgetItem = function(obj, key) {
     $.ajax({
         type: "POST",
         url: "budget.php",
-        data: {"action" : "remove", "key" : key}
+        data: {"action" : "remove", "key" : key},
+        success: function(data) {
+            $("#budgetTable").html(data);
+        }
     });
 
-    $(obj).parents("tr").remove();
+    //$(obj).parents("tr").remove();
 }
