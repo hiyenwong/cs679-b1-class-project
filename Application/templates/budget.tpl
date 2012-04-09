ee8c0a99375ddeb7037a1e0ce93f3d5dc8da699e
jreimels <jreimels@bu.edu>
2012-04-09 00:42:50 +0000

    modified budget.inc and activity.inc getOption methods to return more data added jQueryUI css and js files added new method to Date.inc to get a the date from the 1st of the current month budget page is now fully functional (css changes still needed)

--- Application/templates/budget.tpl
+++ Application/templates/budget.tpl
@@ -7,35 +7,19 @@
 <div id="header_wrap{if !$homepage}_secondary{/if}">
 	<div id="header{if $secondary}_secondary{/if}"><!-- begin header -->
 		{include file='upperheader.tpl' user=$user}
-	  
 	  	{include file='lowerheader.tpl' title='Budgets' message='Modify your budgets'}
 	</div><!-- end header -->
 </div>
-		
+
 <div id="page{if !$homepage}_secondary{/if}" id="budget"><!-- start page content -->
-          
+
 	<div id="main_content">
 		{include file="leftnav.tpl" active='Budget'}
 
-		<div id="main_content_left_secondary">   
-          	<table>
-                <thead>
-                    <tr>
-                        <th>Budget</th>
-                        <th>Spent</th>
-                        <th>&nbsp;</th>
-                    </tr>
-                </thead>
-                <tbody id="budgetRows">                   
-                    {foreach from=$budgets key=k item=v}
-                        <tr>
-                            <td>{$v}</td>
-                            <td>0</td>
-                            <td><input type="button" value="Remove" onclick="removeBudgetItem(this, {$k})" /></td>
-                        </tr>
-                    {/foreach}                    
-                </tbody>
-          	</table>
+		<div id="main_content_left_secondary">
+            <div id="budgetTable">
+                {include file='budget_table.tpl'}
+          	</div>
           	
           	<div id="addBudgetRow">
                 <form>
@@ -55,7 +39,7 @@
                         <p>
                             <label for="addBudgeted">Amount: </label>
                             <input type="text" id="addBudgeted" name="budgetedAmount" />
-                            <span class="error"></span>
+                            <div id="addBudgetError" class="error"></div>
                         </p>
                         <p>
                             <input type="button" value="Save" id="saveBudgetItem" />
--- Application/www/budget.php
+++ Application/www/budget.php
@@ -1,5 +1,4 @@
 <?php
-	session_start();
 	require_once '../includes/config.inc';
 	require_once 'user.inc';
 	require_once 'access.inc';
@@ -9,13 +8,18 @@
     require_once 'factory.inc';
     require_once 'view.inc';
     require_once 'transaction.inc';
+    require_once 'activity.inc';   
+    require_once 'date.inc';
 
 	try {
+	    $fullPage = true;
 		$access = new Access();
 		$access->authenticate();
-		$user = $access->getUser();
+		$user = $access->getUser();	
+        $smarty = new MySmarty($SMARTY_CONFIG);
 		
 	    if (!empty($_POST)) {
+	       $fullPage = false;
 	       $action = htmlentities($_POST['action']);
 	       
 	       if ($action === "add") {
@@ -48,27 +52,42 @@
     	       $transaction->commit();
 	       }
 	          
-	    } else {            		
-    		$smarty = new MySmarty($SMARTY_CONFIG);
-    		
-    		$userCategories = Category::getOptions(array("USER_ID" => $user->getId()));
+	    }      		
+		
+		$budgets = Budget::getOptions(array("USER_ID" => $user->getId(), "ACTIVE" => 1));
+		$spent = array();
+		
+		foreach ($budgets as $key => $item) {
+            $activities = Activity::getOptions(array("USER_ID" => $user->getId(), "CATEGORY_ID" => $item['category_id'], "TRANSACTION_DATE_START" => Date::firstOfMonth()));
+            $value = 0;
+            
+            foreach ($activities as $activityKey => $activityItem) {
+                $value += $activityItem['amount'];
+            }
+            
+            $spent[$key] = $value;
+		}
+		
+		$smarty->assign('spent', $spent);
+		$smarty->assign('budgets', $budgets);
+		
+		if ($fullPage) {
+            $userCategories = Category::getOptions(array("USER_ID" => $user->getId()));
     		$allCategories = Category::getOptions();
     		
-    		$budgets = Budget::getOptions(array("USER_ID" => $user->getId(), "ACTIVE" => 1));
-    		
-    		$smarty->assign('budgets', $budgets);
     		$smarty->assign('allCategories', $allCategories);
     		$smarty->assign('userCategories', $userCategories);
     		$smarty->assign('user', $user);
     		$smarty->assign('left_menu', true);
     		$smarty->display('budget.tpl');
+		} else {
+            echo $smarty->fetch('budget_table.tpl');
 		}
 		
 	} catch (AccessDeniedException $e) {
 		trigger_error('Access Denied');
 	} catch (Exception $e) {
-		//trigger_error('An error has occurred, please try again in a few minutes');
-		print_r($e);
+		trigger_error('An error has occurred, please try again in a few minutes');
 	}
 	
 ?>
