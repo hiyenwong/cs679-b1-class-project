5d30469cffe919b32ebd825612205b3ecb9a19ab
churkl <churkl@gmail.com>
2012-04-15 19:00:16 +0000

    Adding Finishing touches to custom mapping

--- Application/www/custommapping.php
+++ Application/www/custommapping.php
@@ -2,6 +2,12 @@
 require_once '../includes/config.inc';
 require_once 'access.inc';
 require_once 'accessdeniedexception.inc';
+require_once 'transaction.inc';
+require_once 'factory.inc';
+require_once 'view.inc';
+require_once 'user.inc';
+require_once 'mapping.inc';
+require_once 'mappingdetail.inc';
 
 try {
 	$access = new Access();
@@ -27,6 +33,45 @@ try {
 		} catch (Exception $e) {
 			echo json_encode(array('success' => 'no'));
 		}
+		
+	} else if (array_key_exists('submitted', $_REQUEST) && $_REQUEST['submitted'] == 'Save') {
+		$startingRow = isset($_REQUEST['startingRow']) ? htmlentities($_REQUEST['startingRow']) : false;
+		if ($startingRow === false) {
+			throw new Exception('Missing required parameter starting row');
+		}
+		
+		$name = isset($_REQUEST['name']) ? htmlentities($_REQUEST['name']) : false;
+		if ($name === false) {
+			throw new Exception('Missing required parameter custom mapping name');
+		}
+		
+		$transactionDateCol = isset($_REQUEST['transactionDate']) ? htmlentities($_REQUEST['transactionDate']) : false;
+		if ($transactionDateCol === false) {
+			throw new Exception('Missing required parameter transaction date column');
+		}
+		
+		$descriptionCol = isset($_REQUEST['description']) ? htmlentities($_REQUEST['description']) : false;
+		if ($descriptionCol === false) {
+			throw new Exception('Missing required parameter description column');
+		}
+		
+		$amountCol = isset($_REQUEST['amount']) ? htmlentities($_REQUEST['amount']) : false;
+		if ($amountCol === false) {
+			throw new Exception('Missing required parameter amount column');
+		}
+		
+		$transaction = new Transaction(new MySqlDB());
+		$transaction->start();
+		
+		$mapping = Factory::createView(new MappingKey());
+		$mapping->setUser($user);
+		$mapping->setStartingRow($startingRow);
+		$mapping->setName($name);
+		$mapping->addMappingDetail(new MappingDetail($mapping, MappingDetail::$MAPPING_TRANSACTION_DATE, $transactionDateCol));
+		$mapping->addMappingDetail(new MappingDetail($mapping, MappingDetail::$MAPPING_AMOUNT, $amountCol));
+		$mapping->addMappingDetail(new MappingDetail($mapping, MappingDetail::$MAPPING_NAME, $descriptionCol));
+		
+		$transaction->commit();
 		
 	} else {
 		$smarty = new MySmarty($SMARTY_CONFIG);
