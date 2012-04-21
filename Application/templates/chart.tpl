5cea29a4456512ec02cbb81067a2c3c52de92d19
churkl <churkl@gmail.com>
2012-04-21 13:03:38 +0000

    rewritten charts completely from ground up

--- Application/templates/chart.tpl
+++ Application/templates/chart.tpl
@@ -1,16 +1,27 @@
 {include file='header.tpl'}
 
-<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-<script type="text/javascript" src="{$js_url}/chart.js"></script>
-<link type="text/css" href="{$style_url}/ui-lightness/jquery-ui-1.8.18.custom.css" rel="Stylesheet" />
-<script type="text/javascript" src="{$js_url}/jquery-ui-1.8.18.custom.min.js"></script>
-
 {literal}
-<script>
-	$(function() {
-		$( "#tabs" ).tabs();
-	});
-	</script>
+<script type="text/javascript">
+	google.load("visualization", "1", {packages:["corechart"]});
+	google.setOnLoadCallback(drawChart);
+	function drawChart() {
+		var jsonData = $.ajax({
+			url: base_url + "/chart.php?func=getData&chart=pie",
+			dataType:"json",
+			async: false}).responseText;
+		var data = new google.visualization.DataTable(jsonData);
+		var chart = new google.visualization.PieChart(document.getElementById('pie_chart_div'));
+		var formatter = new google.visualization.NumberFormat({prefix: '$', negativeColor: 'red', negativeParens: true});
+		formatter.format(data, 1);
+		var options = {
+          title: 'Past 6 Months Spending',
+          allowHtml: true,
+          width: 400,
+          height: 300
+        };
+		chart.draw(data, options);
+	}
+</script>
 {/literal}
 
 <div id="header_wrap{if !$homepage}_secondary{/if}">
@@ -26,14 +37,12 @@
 	<div id="main_content">
 		{include file="leftnav.tpl" active='Chart'}
 		
-		<div id="main_content_left_secondary">   
-                    
-		</div><!-- end main content left  -->
-		<div id="chart_div" style="width:400; height:300; float:left;"></div>
-		<div id="barchart_div" style="width:400; height:300; float:right; margin-top: -250px"></div>
+		<div id="main_content_left_secondary"></div><!-- end main content left  -->
+		
+		<div id="pie_chart_div"></div>
 		<div style="clear: both;"></div>
       	<!-- end main content right  -->
-    </div> <!-- end main content  -->       
+    </div><!-- end main content  -->       
           
 </div>
 <div class="push"></div>
--- Application/www/chart.php
+++ Application/www/chart.php
@@ -1,28 +1,64 @@
 <?php
-	require_once '../includes/config.inc';
-	require_once 'user.inc';
-	require_once 'access.inc';
-	require_once 'accessdeniedexception.inc';
-    require_once 'category.inc';
-    require_once 'budget.inc';
-    require_once 'factory.inc';
-    require_once 'view.inc';
-    require_once 'transaction.inc';
+require_once '../includes/config.inc';
+require_once 'user.inc';
+require_once 'access.inc';
+require_once 'accessdeniedexception.inc';
+require_once 'activity.inc';
+require_once 'category.inc';
+require_once 'budget.inc';
+require_once 'factory.inc';
+require_once 'view.inc';
+require_once 'transaction.inc';
+require_once 'date.inc';
 
-	try {
-		$access = new Access();
-		$access->authenticate();
-		$user = $access->getUser(); 		
-    	$smarty = new MySmarty($SMARTY_CONFIG);
-    	$smarty->assign('user', $user);
-    	$smarty->assign('left_menu', true);
-    	$smarty->display('chart.tpl');
-    	
-	} catch (AccessDeniedException $e) {
-		trigger_error('Access Denied');
-	} catch (Exception $e) {
-		//trigger_error('An error has occurred, please try again in a few minutes');
-		print_r($e);
+$func = isset($_REQUEST['func']) ? htmlentities($_REQUEST['func']) : false;
+$chart = isset($_REQUEST['chart']) ? htmlentities($_REQUEST['chart']) : false;
+
+try {
+	$access = new Access();
+	$access->authenticate();
+	$user = $access->getUser();
+		
+	if ($func && $chart && $func == 'getData' && $chart == 'pie') {
+		$today = new Date();
+		$firstDayOf6MonthsAgo = new Date($today->format("%Y") . ($today->format("%m") - 6) . '-01');
+	
+		$activities = array();
+		foreach (Activity::getOptions(array(
+					'USER_ID' => $user->getId(),
+					'TRANSACTION_DATE_START' => $firstDayOf6MonthsAgo->format(Date::$SIMPLE_YEAR_MONTH_DAY),
+					'TRANSACTION_DATE_END' => $today->format(Date::$SIMPLE_YEAR_MONTH_DAY))) AS $id => $value) {
+			$transactionDate = new Date($value['transaction_date']);
+			if (array_key_exists($transactionDate->format('%Y-%m'), $activities)) {
+				$activities[$transactionDate->format('%Y-%m')] += $value['amount'];
+			} else {
+				$activities[$transactionDate->format('%Y-%m')] = $value['amount'];
+			}
+		}
+		
+		$results = array();
+		foreach ($activities as $date => $amount) {
+			$results[] = array('c' => array(array('v' => $date), array('v' => abs($amount))));
+		}
+		
+		echo json_encode(array('cols' => array(
+									array('id'=>'transaction_date', 'label'=>'Transaction Date', 'type'=>'string'),
+									array('id'=>'amount', 'Label'=>'Amount', 'type'=>'number')),
+						'rows' => $results));
+	} else {
+		
+		$smarty = new MySmarty($SMARTY_CONFIG);
+		$smarty->assign('user', $user);
+		
+		$smarty->assign('left_menu', true);
+		$smarty->display('chart.tpl');
 	}
+	
+} catch (AccessDeniedException $e) {
+	trigger_error('Access Denied');
+} catch (Exception $e) {
+	//trigger_error('An error has occurred, please try again in a few minutes');
+	print_r($e);
+}
 	
 ?>
