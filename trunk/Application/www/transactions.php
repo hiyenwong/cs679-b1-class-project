70e497fca7592abd612e7efe4801d71f57ec2ae6
churkl@gmail.com <churkl@gmail.com@e7e56146-f8dc-8eb2-db1c-0f49c4865b40>
2012-04-23 23:20:42 +0000

    Edit and calendar image and search is fixed for transactions

--- trunk/Application/www/transactions.php
+++ trunk/Application/www/transactions.php
@@ -1,39 +1,49 @@
 <?php
-	require_once '../includes/config.inc';
-	require_once 'user.inc';
-	require_once 'access.inc';
-	require_once 'activity.inc';
-	require_once 'accessdeniedexception.inc'; 
-	require_once 'category.inc';
-	require_once 'transaction.inc';
-	
-	$access = new Access();
-	$access->authenticate();
-	$user = $access->getUser();
+require_once '../includes/config.inc';
+require_once 'user.inc';
+require_once 'access.inc';
+require_once 'activity.inc';
+require_once 'accessdeniedexception.inc'; 
+require_once 'category.inc';
+require_once 'transaction.inc';
 
-	//var_dump($_POST['data']['transdate']); exit;
-	if (!empty($_POST)) {
-		switch ($_POST['action']) {
+$access = new Access();
+$access->authenticate();
+$user = $access->getUser();
+
+$action = isset($_REQUEST['action']) ? htmlentities($_REQUEST['action']) : false;
+$data = isset($_REQUEST['data']) ? $_REQUEST['data'] : false;
+
+try {
+	if ($action) {
+		if ($data === false) {
+			throw new Exception("Missing required parameter data");
+		}
+		
+		switch ($action) {
 			case 'edit':
 				$transaction = new Transaction(new MySqlDB());
 				$transaction->start();
 				
-				$activity = Factory::getView(new ActivityKey($_POST['data']['id']));
-				$activity->setName($_POST['data']['name']);
+				$activity = Factory::getView(new ActivityKey($data['id']));
+				$activity->setName($data['name']);
 				
-				$activity->setCategory(Factory::getView(new CategoryKey($_POST['data']['category'])));
-				//var_dump($activity->getCategory());exit;
-				$activity->setTransactionDate((date('Y-m-d H:i:s', strtotime($_POST['data']['transdate']))));
-				$activity->setAmount($_POST['data']['amount']);
+				if ($data['category'] != null && trim($data['category']) && $data['category'] != 'null') {
+					$activity->setCategory(Factory::getView(new CategoryKey(trim($data['category']))));
+				}
+				$activity->setTransactionDate(new Date($data['transdatr']));
+				$activity->setAmount($data['amount']);
 				$transaction->commit();
 				break;
 			case 'remove':
-				echo $_POST['data']['id'];
-				print_r($_POST); exit;
+				if (!array_key_exists('id', $data) || !trim($data['id'])) {
+					throw new Exception("Missing required parameter transaction id");
+				}
+				
 				$transaction = new Transaction(new MySqlDB());
 				$transaction->start();
 				
-				$activity = Factory::getView(new ActivityKey($_POST['data']['id']));
+				$activity = Factory::getView(new ActivityKey($data['id']));
 				$activity->delete();
 				$transaction->commit();
 				break;
@@ -41,45 +51,52 @@
 				$transaction = new Transaction(new MySqlDB());
 				$transaction->start();
 				
+				$date = new Date();
+				
 				$activity = Factory::createView(new ActivityKey());
 				$activity->setUser($user);
 				$activity->setImportNumber(-1);
-				$activity->setImportTime(date('Y-m-d'));
-				
-				$activity->setName($_POST['data']['name']);
-				
-				//$activity->setCategory(Factory::getView(new CategoryKey($_POST['data']['category'])));
-				//var_dump($activity->getCategory());exit;
-				$activity->setTransactionDate((date('Y-m-d H:i:s', strtotime($_POST['data']['transdate']))));
+				$activity->setImportTime($date);
+				$activity->setName($data['name']);
+				$activity->setTransactionDate($date);
 				$activity->setAmount($_POST['data']['amount']);
+				
+				if ($data['category'] != null && trim($data['category']) && $data['category'] != 'null') {
+					$activity->setCategory(Factory::getView(new CategoryKey(trim($data['category']))));
+				}
+				
 				$transaction->commit();
 				break;
 		}
-		echo json_encode('success');
-		return;
-	}
-	
-	
-	try {
+		echo json_encode(array('success'=>'true'));
+	} else {
+
 		$smarty = new MySmarty($SMARTY_CONFIG);
 		$smarty->assign('res', $user->getActivities());
-
+	
 	 	$userCategories = Category::getOptions(array("USER_ID" => $user->getId(), "ACTIVE" => 1));
-        $allCategories = Category::getOptions(array("ACTIVE" => 1));
-    	
-        $categories = '';
-        foreach($userCategories as $index=>$value) {
-        	$categories .= "{\"value\": \"$index\", \"label\": \"$value\"},";     		
-        }
-        $smarty->assign('allCategories', $categories);
-    		
+		$allCategories = Category::getOptions(array("ACTIVE" => 1));
+		
+		$categories = '';
+		foreach($userCategories as $index=>$value) {
+			$categories .= "{\"value\": \"$index\", \"label\": \"$value\"},";	 		
+		}
+		$smarty->assign('allCategories', $categories);
+			
 		$smarty->assign('user', $user);
 		$smarty->assign('left_menu', true);
 		$smarty->assign('BASE_URL', $BASE_URL);
 		$smarty->display('transactions.tpl');
-	} catch (AccessDeniedException $e) {
-		
-	} catch (Exception $e) {
-		
 	}
+	
+} catch (AccessDeniedException $e) {
+	header ('HTTP/1.1 401 Access Denied');
+	echo "AccessDeniedException: " . $e->getMessage ();
+} catch (Exception $e) {
+	if ($transaction && !$transaction->isComplete ()) {
+		$transaction->rollback ();
+	}
+	header ('HTTP/1.1 500 Internal Server Error');
+	echo "Exception: " . $e->getMessage ();
+}
 ?>
